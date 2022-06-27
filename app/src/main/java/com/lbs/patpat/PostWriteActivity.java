c41f52a0237e39a8d9d34bb2b64302f6d379f7c3
kudou-lbs <3514633170@qq.com>
2022-06-27 17:23:42 +0800

    更新论坛背景、写帖子活动

---
+++ app/src/main/java/com/lbs/patpat/PostWriteActivity.java
@@ -0,0 +1,14 @@
+package com.lbs.patpat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PostWriteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_write);
+    }
+}
